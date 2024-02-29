f0a1cf61728005198b12d1b32d678208dfa174c9
abdel <abdelaziz.bensenouci2023@campus-eni.fr>
2024-02-29 15:01:17 +0100

    Securité

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-   function index(Request $request): response {
-        return $this->render('home/index.html.twig');
+   function index(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $hasher): response {
+     return $this->render('home/index.html.twig');
    }
 }
--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class RecipeController extends AbstractController
