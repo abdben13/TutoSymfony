9f957802ccb13d28041e5e673eb60fd4a04de33f
abdel <abdelaziz.bensenouci2023@campus-eni.fr>
2024-03-05 15:09:06 +0100

    Catégorie

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -2,23 +2,15 @@
 
 namespace App\Form;
 
-use App\Entity\Recipe;
-use Symfony\Component\Form\FormEvent;
+use App\Entity\Category;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Event\PreSubmitEvent;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Event\PostSubmitEvent;
-use Symfony\Component\String\Slugger\AsciiSlugger;
-use Symfony\Component\Validator\Constraints\Regex;
-use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Validator\Constraints\Sequentially;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
-class RecipeType extends AbstractType
+class CategoryType extends AbstractType
 {
     public function __construct(private FormListenerFactory $listenerFactory)
     {
@@ -28,20 +20,17 @@ class RecipeType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', TextType::class, [
+            ->add('name', TextType::class, [
                 'empty_data' => ''
             ])
             ->add('slug', TextType::class, [
-                'required' => false   
-            ])
-            ->add('content', TextareaType::class, [
+                'required'=> false,
                 'empty_data' => ''
             ])
-            ->add('duration')
             ->add('save', SubmitType::class, [
-                'label' => 'Envoyer'
+                'label' => 'Enregistrer'
             ])
-            ->addEventListener(FormEvents::PRE_SUBMIT, $this->listenerFactory->autoSlug('title'))
+            ->addEventListener(FormEvents::PRE_SUBMIT, $this->listenerFactory->autoSlug('name'))
             ->addEventListener(FormEvents::POST_SUBMIT, $this->listenerFactory->timestemp())
         ;
     }
@@ -49,7 +38,7 @@ class RecipeType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Recipe::class,
+            'data_class' => Category::class,
         ]);
     }
 }
